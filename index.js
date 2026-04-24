bb78b2ad01bf0c3c26406ffbec61b8188c69e905
Allysson2023 <allyssoncarlos.ac21@gmail.com>
2026-04-23 21:33:19 -0300

    atualizando o javascript clica no botao, pega Id, busca produto, adicionar no carrinho, atualiza lista, calcular total, permite remover

--- index.js
+++ index.js
@@ -1,31 +1,61 @@
+const produtos = [
+    { id: 1, nome: "Hamburguer", preco: 10},
+    { id: 2, nome: "Refrigerante", preco: 8},
+    { id: 3, nome: "Pizza", preco: 20}
+];
+
+const botoes = document.querySelectorAll(".add-btn");
+
+botoes.forEach(botao => {
+    botao.addEventListener("click", ()=> {
+        const id = botao.getAttribute("data-id");
+        adicionarItemPorId(id);
+    })
+})
+
+function adicionarItemPorId(id){
+    const produto = produtos.find( p => p.id == id);
+
+    adicionarItem(produto);
+}
+
+function formatarMoeda(valor){
+    return valor.toFixed(2).replace(".", ",");
+}
+
 let carrinho = [];
 let total = 0;
 
-function adicionarItem(nome, preco){
-
+function adicionarItem(produto){
     const itemExistente = carrinho.find(
-        item => item.nome === nome);
+        item => item.id === produto.id
+    );
 
-    if (itemExistente){
-        itemExistente.quatidade += 1;
-    }else{
-        carrinho.push({ nome, preco, quatidade: 1});
-
+    if(itemExistente){
+        itemExistente.quantidade++;
+    } else{
+        carrinho.push({
+            id: produto.id,
+            nome : produto.nome,
+            preco : produto.preco,
+            quantidade : 1
+        });
     }
-
-    total += preco;
+    total += produto.preco
 
     atualizarCarrinho();
+
 }
 
-function excluirItem(nome){
+
+function excluirItem(id){
     const index = carrinho.findIndex(
-        item => item.nome === nome);
+        item => item.id === id);
 
     if( index !== -1){
 
-        if(carrinho[index].quatidade > 1){
-            carrinho[index].quatidade -= 1;
+        if(carrinho[index].quantidade > 1){
+            carrinho[index].quantidade--;
             total -= carrinho[index].preco;
         } else{
             total -= carrinho[index].preco;
@@ -44,12 +74,15 @@ function atualizarCarrinho(){
     carrinho.forEach(item => {
         const li = document.createElement("li");
 
-        const subtotal = item.preco * item.quatidade;
+        const subtotal = item.preco * item.quantidade;
 
-        li.textContent = `${item.nome} x${item.quatidade} - R$ ${subtotal}`;
+        li.innerHTML = `
+            ${item.nome} x ${item.quantidade} - R$ ${formatarMoeda(subtotal)}
+            <button onclick = "excluirItem(${item.id})">❌</button>
+        `;
 
         lista.appendChild(li);
     });
 
-    document.getElementById("total").textContent = total;
+    document.getElementById("total").textContent = formatarMoeda(total);
 }
